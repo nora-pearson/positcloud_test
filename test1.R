e05a451faaf9b22ba7a6fe58237c83d47cdf7cde
Nora Pearson <pear0747@umn.edu>
2023-11-02 19:40:43 +0000

    more specific token instructions

--- test1.R
+++ test1.R
@@ -2,6 +2,7 @@
 #github account linked to posit cloud - click name in posit cloud - authentication - select git (will have to log in)
 #https://stackoverflow.com/questions/74621376/creating-new-project-in-posit-rstudio-cloud-from-private-github-repository
 #following above instructions. created new personal access token in git, said it was for posit, pasted into obsidian
+      #settings -> developer settings -> personal access tokens -> tokens (classic) -> generate new token
 
 #in git, created a new repo "positcloud_test"
 #in posit cloud, created a new project from git repo, pulled in "positcloud test", made these notes in a new R script
